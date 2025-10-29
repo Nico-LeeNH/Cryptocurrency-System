d25a578abf8b966f942417f4408232a222077539
Nico Lee <new.nikolee2007@gmail.com>
2025-10-29 17:39:16 +0700

    style: update format code

--- Cryptocurrency/TxHandler.java
+++ Cryptocurrency/TxHandler.java
@@ -5,28 +5,11 @@ import java.util.*;
 
 public class TxHandler {
 
-    /**
-     * Creates a public ledger whose current UTXOPool (collection of unspent
-     * transaction outputs) is
-     * {@code utxoPool}. This should make a copy of utxoPool by using the
-     * UTXOPool(UTXOPool uPool)
-     * constructor.
-     */
     private UTXOPool utxoPool;
     public TxHandler(UTXOPool utxoPool) {
         this.utxoPool = new UTXOPool(utxoPool);
     }
 
-    /**
-     * @return true if:
-     *         (1) all outputs claimed by {@code tx} are in the current UTXO pool,
-     *         (2) the signatures on each input of {@code tx} are valid,
-     *         (3) no UTXO is claimed multiple times by {@code tx},
-     *         (4) all of {@code tx}s output values are non-negative, and
-     *         (5) the sum of {@code tx}s input values is greater than or equal to
-     *         the sum of its output
-     *         values; and false otherwise.
-     */
     public boolean isValidTx(Transaction tx) {
         double input = 0;
         double output = 0;
@@ -42,10 +25,8 @@ public class TxHandler {
             Transaction.Output prevOutput = utxoPool.getTxOutput(utxo);
 
             PublicKey pubKey = prevOutput.address;
-            byte[] rawData = tx.getRawDataToSign(i);
-            byte[] signature = in.signature;
 
-            if (!Crypto.verifySignature(pubKey, rawData, signature))
+            if (!Crypto.verifySignature(pubKey, tx.getRawDataToSign(i), in.signature))
                 return false;
 
             if (usedUtxos.contains(utxo))
@@ -67,11 +48,6 @@ public class TxHandler {
         return true;
     }
 
-    /**
-     * Handles each epoch by receiving an unordered array of proposed transactions, checking each
-     * transaction for correctness, returning a mutually valid array of accepted transactions, and
-     * updating the current UTXO pool as appropriate.
-     */
     public Transaction[] handleTxs(Transaction[] possibleTxs) {
         ArrayList<Transaction> accTxs = new ArrayList<>();
         boolean update = true;
